81729d11ba85d1b7865607034592a7b51741c540
gael <gael.proteau@recommerce.com>
2024-02-29 00:02:51 +0100

    Support Chinese modes

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
 const url = require('url');
 const axios = require('axios');
 
-// From Heatzy API : https://drive.google.com/drive/folders/0B9nVzuTl4YMOaXAzRnRhdXVma1k
+// From Heatzy API: https://drive.google.com/drive/folders/0B9nVzuTl4YMOaXAzRnRhdXVma1k
 // https://heatzy.com/blog/tout-sur-heatzy
 const heatzyUrl = "https://euapi.gizwits.com/app/";
 const loginUrl = 'https://euapi.gizwits.com/app/login';
 const heatzyApplicationId = "c70a66ff039d41b4a220e198b0fcc8b3";
 
 // The following values are used in the API to get the mode. (To get the mode, the API returns of these strings)
-const validModes = new Set(['cft', 'eco', 'fro', 'off']); // Known values for the Heatzy mode
+const validModes = ['cft', 'eco', 'fro', 'off']; // Known values for the Heatzy mode
+const validModesChinese = ['舒适','经济','解冻','停止'];
+
 // The following values are used in the API to set the mode. (To set the mode, the API needs one of these values)
 const cft = 0; // Comfort mode
 const eco = 1; // Eco mode
@@ -27,12 +29,12 @@ module.exports = (homebridge) => {
 		'homebridge-heatzy-as-switch',
 		'HeatzyAsSwitch',
 		SwitchAccessory
-	)
+	);
 };
 
 function SwitchAccessory(log, config) {
-    this.log = log
-    this.config = config
+    this.log = log;
+    this.config = config;
 
 	// Config
     this.getUrl = url.parse(heatzyUrl + "devdata/" + config["did"] + "/latest");
@@ -44,11 +46,11 @@ function SwitchAccessory(log, config) {
     this.trace = config["trace"] || false;
 
     this.switchOn = config["switchOn"] || "cft"; // default value is comfort mode
-    if (!(validModes.has(this.switchOn))) { // if the set value is invalid, set it to default
+    if (!validModes.includes(this.switchOn)) {
         this.switchOn = "cft";
     }
     this.switchOff = config["switchOff"] || "eco"; // default value is eco mode
-    if (!(validModes.has(this.switchOff))) { // if the set value is invalid, set it to default
+    if (!validModes.includes(this.switchOff)) {
         this.switchOff = "eco";
     }
 
@@ -74,7 +76,7 @@ function SwitchAccessory(log, config) {
         .on('set', this.setOnCharacteristicHandler.bind(this));
 
     this.updateState(); // Get the current state of the device, and update HomeKit
-    setInterval(this.updateState.bind(this), 1000 * this.interval) // The state of the device will be checked at this interval (in sec)
+    setInterval(this.updateState.bind(this), 1000 * this.interval); // The state of the device will be checked at this interval (in sec)
 
     this.log("Starting HeatzyAsSwitch...");
 }
@@ -100,23 +102,21 @@ async function updateToken(device) {
             device.heatzyToken = response.data.token;
             device.heatzyTokenExpireAt = 1000 * response.data.expire_at; // The API returns a date in seconds, but javascript works in ms...
             if (device.trace) {
-                device.log('Logged in Heatzy server.')
+                device.log('Logged in Heatzy server.');
             }
         } else {
             device.log('Error at login: ' + response.status + ' ' + response.statusText + ' ' + response.data.error_message);
         }
     } catch (error) {
-        device.log('Error - Unable to log in to Heatzy server');
+        device.log('Error: Unable to log in to Heatzy server');
         device.log('Error message: ', error.message);
     }
 }
 
 async function getState(device) { //return the state of the device as a boolean. Or null if undefined
     if (device.heatzyTokenExpireAt < Date.now()) {
-        await updateToken(device)
+        await updateToken(device);
     }
-
-    let state = false;
 
     try {
         const response = await axios({
@@ -130,48 +130,50 @@ async function getState(device) { //return the state of the device as a boolean.
             },
         });
 
-        if (response.status == 200) {
-            if (response.data.attr.mode === device.switchOn) {
-                state = true
-            } // All modes which are not this.switchOn will turn off the switch
-        } else {
+        if (response.status != 200) {
             device.log('Error: ' + response.status + ' ' + response.statusText + ' ' + response.data.error_message);
-            state = null
+            return null;
         }
+
+        let currentMode = response.data.attr.mode;
+
+        if (validModesChinese.includes(currentMode)) {
+            currentMode = validModes[validModesChinese.indexOf(currentMode)];
+        }
+
+        return currentMode === device.switchOn;
     } catch (error) {
         device.log('Error when getting state: ' + error.code);
-        state = null
+        return null;
     }
-
-    return state;
 }
 
 async function setState(device, state) { // Set the state of the device, and return it if successful. Or null if failed
     if (device.heatzyTokenExpireAt < Date.now()) {
-        await updateToken(device)
+        await updateToken(device);
     }
 
-    // Let set mode to the value of the homekit switch
+    // Set mode to the value of the homekit switch
     let mode = device.switchOff;
 
     if (state) {
-        mode = device.switchOn
+        mode = device.switchOn;
     }
 
     // The API works with literal modes (in 2023), but the specification is to use a numeric value. So we convert it.
     let modeNum = 0;
 
     switch (mode) {
-        case "cft" :
+        case "cft":
             modeNum = cft;
             break;
-        case "eco" :
+        case "eco":
             modeNum = eco;
             break;
-        case "fro" :
+        case "fro":
             modeNum = fro;
             break;
-        case "off" :
+        case "off":
             modeNum = off;
     }
 
@@ -192,14 +194,14 @@ async function setState(device, state) { // Set the state of the device, and ret
 
         if (response.status != 200) {
             device.log('Error: ' + response.status + ' ' + response.statusText + ' ' + response.data.error_message);
-            state = null
+            state = null;
         }
     } catch (error) {
         device.log('Error when setting state: ' + error.code);
-        state = null
+        state = null;
     }
 
-    return state
+    return state;
 }
 
 SwitchAccessory.prototype.updateState = async function () {
@@ -208,7 +210,7 @@ SwitchAccessory.prototype.updateState = async function () {
     if (state !== null) {
         if (this.state === null) {
             // Initialize for first run
-            this.state = state
+            this.state = state;
         }
 
         if (state !== this.state) {
@@ -228,7 +230,7 @@ SwitchAccessory.prototype.getOnCharacteristicHandler = async function (callback)
     let state = await getState(this);
 
     if (this.trace) {
-        this.log('HomeKit asked for state (true for on, false for off): ' + state)
+        this.log('HomeKit asked for state (true for on, false for off): ' + state);
     }
 
     if (state != null) {
@@ -243,7 +245,7 @@ SwitchAccessory.prototype.setOnCharacteristicHandler = async function (value, ca
     let state = await setState(this, value);
 
     if (this.trace) {
-        this.log('HomeKit changed state to (true for on, false for off): ' + state)
+        this.log('HomeKit changed state to (true for on, false for off): ' + state);
     }
 
     //  This code only works when the new state is correctly reflected on Heatzy servers.
@@ -258,6 +260,6 @@ SwitchAccessory.prototype.setOnCharacteristicHandler = async function (value, ca
 };
 
 SwitchAccessory.prototype.getServices = function () {
-    this.log("Init services...")
+    this.log("Init services...");
     return [this.service, this.informationService];
 };
